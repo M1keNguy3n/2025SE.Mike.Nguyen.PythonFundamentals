780021a685d2049d43058e895e01d17ca8a0d967
M1keNguy3n <158791704+M1keNguy3n@users.noreply.github.com>
2024-02-07 01:02:27 +0000

    Completed

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,14 @@
+str = input("Enter the file name: ")
+match str[-4::]:
+    case ".jpg":
+        print("image/jpg")
+    case ".jpeg":
+        print("image/jpeg")
+    case ".txt":
+        print("text/txt")
+    case ".pdf":
+        print("document/pdf")
+    case ".zip":
+        print("compressed/zip")
+    case ".png":
+        print("image/png")
