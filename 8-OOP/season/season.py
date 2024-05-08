878a19a29c2cbe6841fce86905f655e20b858b98
Mike N <158791704+M1keNguy3n@users.noreply.github.com>
2024-05-08 22:59:10 +0000

    Finished 8.1

--- 8-OOP/season/season.py
+++ 8-OOP/season/season.py
@@ -5,6 +5,7 @@ p = inflect.engine()
 # userbday = input("Enter your birthday (YYYY-MM-DD): ")
 userbday = "2021-05-09"
 datetime_birthday = datetime.fromisoformat(userbday)
-minutes = (datetime.fromisoformat("2023-05-09") - datetime_birthday).days * 24 * 60
+delta_days = (datetime.fromisoformat("2023-05-09") - datetime_birthday).days
+minutes = delta_days * 24 * 60
 print(minutes)
 print(p.number_to_words(minutes, andword="").capitalize(), "minutes.")
