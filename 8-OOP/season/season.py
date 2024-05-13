36aeea29c5d75a3cc2f37f6525d80a1b54c6f07b
Mike N <158791704+M1keNguy3n@users.noreply.github.com>
2024-05-13 02:21:42 +0000

    Added IO to season.py

--- 8-OOP/season/season.py
+++ 8-OOP/season/season.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from datetime import date
+
 import inflect
 
 p = inflect.engine()
-# userbday = input("Enter your birthday (YYYY-MM-DD): ")
-userbday = "2021-05-09"
+userbday = input("Enter your birthday (YYYY-MM-DD): ")
 datetime_birthday = datetime.fromisoformat(userbday)
-delta_days = (datetime.fromisoformat("2023-05-09") - datetime_birthday).days
+delta_days = (
+    datetime.combine(date.today(), datetime.min.time()) - datetime_birthday
+).days
 minutes = delta_days * 24 * 60
 print(minutes)
 print(p.number_to_words(minutes, andword="").capitalize(), "minutes.")
